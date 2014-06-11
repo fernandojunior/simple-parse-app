5a2b7b384527377f2c25c74d37290e1c1cf5b916
fernandojunior <fernandojr.ifcg@live.com>
2014-06-11 04:36:17 -0300

    facebook
    facebook user repository

--- www/repositories/parse.js
+++ www/repositories/parse.js
@@ -1,10 +1,10 @@
 /**
-* Repositorio de objetos do Parse.org. Baseado no design partner Repository.
-* https://github.com/fernandojunior/repository.js
+* Repositorio de objetos do Parse.org 
+* https://parse.com/docs/js_guide
 **/
 var ParseRepository = PrototypeClass.extend({
 
-   prototype: {
+    prototype: {
        
        /**
        * Repository initializer
@@ -18,11 +18,11 @@ var ParseRepository = PrototypeClass.extend({
            if (typeof this._class_name !== "undefined"){
                this._class = Parse.Object.extend(this._class_name);
            }
-
+           
        },
        
        /**
-       * Parse query object for the parse class
+       * Parse query object for the class
        **/
        _query: function(){
            return new Parse.Query(this._class);
@@ -171,7 +171,7 @@ var ParseRepository = PrototypeClass.extend({
    },
        
    /**
-   * Class "static" method for Callback and error callback handler
+   * Callback and error callback handler
    **/
    _handler : function (log, callback, error_callback){
        var calls = {
@@ -199,3 +199,221 @@ var ParseRepository = PrototypeClass.extend({
    **/
    g: {}
 });
+
+/**
+* Repositorio de usuarios parse. Baseado nas classes especiais Parse.User e Parse.FacebookUtils
+**/
+ParseRepository.User = ParseRepository.extend({
+
+    prototype: {
+
+        initialize: function (){
+            this._class = Parse.User;
+        },
+
+        /**
+        * Create a new user. Before it does this, it also checks to make sure that both the username and email are unique
+        * @param args.username Username
+        * @param args.password Encrypeted password
+        * @param args.email User email
+        * @param args... Any data do you want to save
+        **/
+        signup : function(args, callback, error_callback) {            
+            var data = args;
+            var user = this._create();
+            var log = "Signing up a user.";
+            user.signUp(data, ParseRepository._handler(log, callback, error_callback));
+            return;
+        },
+
+        /**
+        * Redirect to signup method
+        **/
+        post : function(args, callback, error_callback){
+            return this.signUp(args, callback, error_callback);
+        },
+
+        /**
+        * Log the user in the app. Traditional mode.
+        * @param args.username Username
+        * @param args.password html input value Encrypted password
+        **/
+        login : function(args, callback, error_callback){
+            var username = args.username;
+            var password = args.password;            
+            var log = "Logging the user in. Traditional mode.";
+
+            this._class.logIn(
+                username,
+                password,
+                ParseRepository._handler(log, callback, error_callback)
+            );
+
+            return;
+
+        },
+
+        /**
+        * Log out the current user
+        **/
+        logout: function (){
+            this._class.logOut();
+            console.log("User was logged out.");
+        },
+        
+        /**
+        * Get the current user that is logged in the session (localStorage).
+        **/
+        current: function(callback, error_callback){
+            var current_user = this._class.current();
+
+            if (current_user) {
+
+                console.log("getting current user");
+
+                if (callback !== null && typeof callback !== "undefined"){
+                    callback(current_user);
+                }
+
+            } else {
+
+                console.log("User is not logged in.");
+
+                if (error_callback !== null && typeof error_callback !== "undefined"){
+                    error_callback();
+                }
+            }
+
+            return;
+        },
+
+        /**
+        * Request a password reset to parse.org
+        * @param args.email The user email needed to reset the password
+        **/
+        reset_password : function(args, callback, error_callback){
+            var email = args.email;
+            var log = "Sending a request for reset the user passaword.";
+            this._class.requestPasswordReset(
+                email,
+                ParseRepository._handler(log, callback, error_callback)
+            );
+            return;
+        }
+    }
+
+});
+
+/**
+* Repositorio de usuarios parse. Baseado nas classes especiais Parse.User e Parse.FacebookUtils
+**/
+ParseRepository.FacebookUser = ParseRepository.User.extend({
+
+    prototype: {
+
+        initialize: function (){
+            this._util = Parse.FacebookUtils;
+        },
+
+        /**
+        * @see ParseRepository.FacebookUser.login
+        **/
+        signup : function(args, callback, error_callback) {
+            return this.login(args, callback, error_callback);
+        },
+
+        /**
+        * Redirect to signup method
+        **/
+        post : function(args, callback, error_callback){
+            return this.login(args, callback, error_callback);
+        },
+
+        /**
+        * Login or sign up a user through Facebook
+        * @param args.permisions Facebook permissions
+        **/
+        login: function(args, callback, error_callback){
+            
+            console.log("ate aqui");
+            
+            if (typeof args === "undefined"){
+                args = {};
+            }
+
+            var permissions = args.permissions;
+            var log = "Logging or signing up a user through Facebook";
+            
+            if (typeof permissions === "undefined"){
+                permissions = "user_likes,email";
+            }
+
+            function login_callback(response){
+
+                if (!response.existed()) {
+                    console.log("User signed up and logged in through Facebook.");
+                } else {
+                    console.log("User just logged in through Facebook!");
+                }
+
+                if (callback !== null && typeof callback !== "undefined"){
+                    callback(response);
+                }
+
+            };
+
+            this._util.logIn(
+                permissions,
+                ParseRepository._handler(log, login_callback, error_callback)
+            );
+            return;
+        },
+
+        /**
+        * Associate an existing Parse.User to a Facebook account
+        * @param args.id User ID
+        **/
+        link: function(args, callback, error_callback){            
+            var id = args.id;
+            var permissions = args.permissions;
+            var log = "Linking parser user from facebook.";
+            
+            function get_callback(response){
+                if (!this._util.isLinked(response)) {
+
+                    this._util.link(
+                        response,
+                        permissions,
+                        ParseRepository._handler(log, callback, error_callback)
+                    );
+                }
+            }
+            
+            this.get({id: id}, get_callback, error_callback);
+            return;
+        },
+
+        /**
+        * Unassociate an existing Parse.User to a Facebook account
+        * @param args.id User object ID
+        **/
+        unlink: function(args, callback, error_callback){
+            var id = args.id;
+            var log = "Unlinking parser user from facebook.";
+
+            function get_callback(response){
+                this._util.unlink(
+                    response, ParseRepository._handler(log, callback, error_callback)
+                );                
+            }
+
+            this.get({id: id}, get_callback, error_callback);
+            return;
+        }
+
+    }
+
+});
+
+ParseRepository.g.user = ParseRepository.User.create();
+ParseRepository.g.facebook_user = ParseRepository.FacebookUser.create();
